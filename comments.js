bba8bf78b3d4d942a7304f98635f0d5e5abfb43d
Sampath Reddy <sampath.13@gmail.com>
2025-01-14 09:56:08 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// create web server and listen to it
+// create a route for /comments
+// when a request is made to /comments, send the comments array as a response
+// to run the server, type node comments.js in the terminal
+// open the browser and type localhost:3000/comments
+// you should see the comments array as a response
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(comments));
+});
+server.listen(3000, '
